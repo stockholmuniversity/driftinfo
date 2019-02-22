eb621e2bd6b7d70dc08a818e3fd216bdfa56e917
usk1129 <32932279+usk1129@users.noreply.github.com>
2019-02-22 09:21:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import yaml 
+
+config_file = 'config_file.yml'
+with open(config_file,'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/usko/flask/venv/app/driftinfo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = cfg['driftinfo_for_database']['path_to_database']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
