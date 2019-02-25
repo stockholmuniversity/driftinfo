7e5bcbea5b30151ff983e8c212126c4c221e465e
Mikael Nordin <mikael.nordin@su.se>
2019-02-25 14:35:55 +0100

    Add mod_proxy stuff

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,8 @@ fi
 HOST=$(hostname --fqdn)
 apt-get update
 apt-get -y install python3 python3-venv sqlite3 apache2 certbot python-certbot-apache 
+a2enmod proxy
+systemctl restart apache2
 
 BASEDIR="/local/driftinfo"
 venv="${BASEDIR}/venv"
