5554cb2ea45f0c8def7b6daa7d3dd91c7be14cd3
Mikael Nordin <mikael.nordin@su.se>
2019-03-06 16:36:02 +0100

    Add locale

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,6 @@ BASEDIR="/local/driftinfo"
 venv="${BASEDIR}/venv"
 source "${venv}/bin/activate"
 export FLASK_APP=${venv}/app/main.py
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 flask run >> /local/driftinfo/logs/driftinfo.log 2>&1 &
