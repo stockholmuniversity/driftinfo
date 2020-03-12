2741f2a742554291269ca6ec444a57d00c644f08
Claes Johansson <claes.johansson@su.se>
2020-03-12 13:24:40 +0100

    MARVAL-1527497: Moved a bit of conf from source to conf, because we were changing them and we might want to do this again sometime.

--- plugins/generic_email.py
+++ plugins/generic_email.py
@@ -10,8 +10,8 @@ import yaml
 service = sys.argv[1]
 valid_services = []
 config_file = '/local/driftinfo/conf/config_file.yml'
-with open(config_file,'r') as ymlfile:
-    cfg = yaml.load(ymlfile,Loader=yaml.SafeLoader)
+with open(config_file, 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
 
 for obj in cfg['plugins']:
     for plugin in obj:
@@ -28,10 +28,12 @@ if service not in valid_services:
     print(msg)
     sys.exit(1)
 
-def send_email(headline,message):
+def send_email(headline, message):
     email = cfg['email']['email_from']
     password = cfg['email']['password']
-    send_to_email = cfg['email']['send_to_email_' + service ]
+    smtp_address = cfg['email']['smtp_server']
+    smtp_port = cfg['email']['smtp_port']
+    send_to_email = cfg['email']['send_to_email_' + service]
     msg = MIMEMultipart()
     msg['From'] = email
     msg['To'] = send_to_email
@@ -39,7 +41,7 @@ def send_email(headline,message):
 
     msg.attach(MIMEText(message, 'plain'))
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP(smtp_address, smtp_port)
     server.starttls()
     server.login(email, password)
     text = msg.as_string()
@@ -47,28 +49,35 @@ def send_email(headline,message):
     server.quit()
 
 def connect_to_db():
-    print( """ Connect to database """)
+    print(""" Connect to database """)
     try:
         conn = sqlite3.connect(cfg['database']['path'])
 
         now = datetime.now()
         dtime = now.strftime("%d/%m/%Y %H:%M:%S")
-        sql_select_Query = "select id,brief_text,headline,long_text,categories from driftinfo where processed_" + service +" = 0"
+        sql_select_query = "select id,brief_text,headline,long_text,\
+                            categories from driftinfo where processed_" +\
+                            service + " = 0"
         if service in disturbance_only:
-            sql_select_Query += " AND disturbance = 1" 
+            sql_select_query += " AND disturbance = 1"
         cursor = conn.cursor()
-        cursor.execute(sql_select_Query)
+        cursor.execute(sql_select_query)
         records = cursor.fetchall()
-        print("Total of unprocessed messages for " + service + " in driftinfo is ", len(records))
+        print("Total of unprocessed messages for " +\
+              service +\
+              " in driftinfo is ",\
+              len(records))
         for row in records:
             message = row[3]
-            if service == "wordpress" :
+            if service == "wordpress":
                 message += "\n[category "+ row[4] + "]"
             if service in use_brief_text:
                 message = row[1]
-            send_email(row[2],message)
-            sql_update_Query = "update driftinfo set processed_" + service + " =\""+ str(dtime) + "\" where id = " + str(row[0])
-            cursor.execute(sql_update_Query)
+            send_email(row[2], message)
+            sql_update_query = "update driftinfo set processed_" + service + \
+                               " =\""+ str(dtime) + "\" where id = " + \
+                               str(row[0])
+            cursor.execute(sql_update_query)
             conn.commit()
         cursor.close()
     except ConnectionError as error:
